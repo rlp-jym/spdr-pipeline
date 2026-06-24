0009fabe2f3519c4ae4bf8d830fbd275f313f7b0
rlp-jym <ralpj.jayoma@gmail.com>
2026-06-25 02:46:23 +0800

    finalized terraform setup

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "5.6.0"
     }
   }
@@ -9,13 +9,12 @@ terraform {
 
 provider "google" {
   project = var.project_name
-  region = var.provider_region
+  region  = var.provider_region
 }
 
-
 resource "google_storage_bucket" "spdr-etfs" {
-  name = var.bucket_name
-  location = var.resource_location
+  name          = var.bucket_name
+  location      = var.resource_location
   force_destroy = true
 
   lifecycle_rule {
@@ -29,7 +28,7 @@ resource "google_storage_bucket" "spdr-etfs" {
 }
 
 resource "google_bigquery_dataset" "spdr-etfs" {
-  dataset_id = var.dataset_id
-  location = var.resource_location
+  dataset_id                 = var.dataset_id
+  location                   = var.resource_location
   delete_contents_on_destroy = false
 }
--- variables.tf
+++ variables.tf
@@ -1,29 +1,29 @@
 variable "project_name" {
-    default = "rlp-jym-projects"
-    description = "The GCP project name where resources will be created."
-    type = string
+  description = "The GCP project name where resources will be created."
+  default     = "rlp-jym-projects"
+  type        = string
 }
 
 variable "provider_region" {
-    default = "asia-southeast1"
-    description = "The GCP region where resources will be created."
-    type = string
+  description = "The GCP region where resources will be created."
+  default     = "asia-southeast1"
+  type        = string
 }
 
 variable "bucket_name" {
-    default = "rlp-jym-projects-spdr-etfs"
-    description = "The name of the storage bucket to create."   
-    type = string
+  description = "The name of the storage bucket to create."
+  default     = "rlp-jym-projects-spdr-etfs"
+  type        = string
 }
 
 variable "dataset_id" {
-    default = "spdr_etfs"
-    description = "The ID of the BigQuery dataset to create."
-    type = string
+  description = "The ID of the BigQuery dataset to create."
+  default     = "spdr_etfs"
+  type        = string
 }
 
 variable "resource_location" {
-    default = "US"
-    description = "The location of the resources to create."
-    type = string
+  description = "The location of the resources to create."
+  default     = "US"
+  type        = string
 }
